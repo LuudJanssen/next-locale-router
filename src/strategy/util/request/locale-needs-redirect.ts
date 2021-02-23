7ede1e02904b87326907218550a1662f355636b6
Luud Janssen <luud-@live.nl>
2021-02-23 17:41:33 +0100

    fix: expect other routes than "/" on domains

--- src/strategy/util/request/locale-needs-redirect.ts
+++ src/strategy/util/request/locale-needs-redirect.ts
@@ -5,5 +5,5 @@ import { getSubpathByLocale } from "../../../util/get-subpath-by-locale"
 export const localeNeedsRedirect = (url: URL, domain: IDomain, locale: string) => {
   // We can assume we only get a locale that's valid for the domain
   const subpath = getSubpathByLocale([domain], locale)!
-  return url.pathname !== subpath?.path
+  return !url.pathname.startsWith(subpath.path)
 }
