2bb62a16ed599bae857f87ac9b2bec4680086dec
Luud Janssen <luud-@live.nl>
2021-03-05 11:29:52 +0100

    build: add build files

--- lib/strategy/util/url/update-pathname-with-redirect.d.ts
+++ lib/strategy/util/url/update-pathname-with-redirect.d.ts
@@ -1,2 +1,2 @@
 import { IRedirect } from "../../../util/redirect.interface";
-export declare const updatePathnameWithRedirect: (pathname: string | null, redirect: IRedirect) => string;
+export declare const updatePathnameWithRedirect: (pathname: string | undefined | null, redirect: IRedirect) => string;
