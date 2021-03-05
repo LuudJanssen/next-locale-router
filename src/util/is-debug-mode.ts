ad175c4a94148d28b8e52bd88d4cbbf1ee8311b0
Luud Janssen <luud-@live.nl>
2021-03-05 12:58:53 +0100

    fix: update debug mode to allow debug config value

--- src/util/is-debug-mode.ts
+++ src/util/is-debug-mode.ts
@@ -2,10 +2,5 @@ import config from "../config"
 
 export const isDebugMode = (): boolean => {
   const debugFlag = process.env.NEXT_LOCALE_ROUTER_DEBUG
-
-  if (typeof debugFlag === "undefined") {
-    return false
-  }
-
-  return process.env.NEXT_LOCALE_ROUTER_DEBUG === "true" || config.debug === true
+  return debugFlag === "true" || config.debug === true
 }
