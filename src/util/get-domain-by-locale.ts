62c517522f4d411186e07d226fe18d776f45170b
Luud Janssen <luud-@live.nl>
2021-04-14 16:34:29 +0200

    fix: remove logger from utility used by client-side

--- src/util/get-domain-by-locale.ts
+++ src/util/get-domain-by-locale.ts
@@ -1,14 +1,11 @@
 import { IDomain } from "../domain.interface"
-import { logger } from "../logger"
 import { domainContainsLocale } from "./domain-contains-locale"
 
 export const getDomainByLocale = (domains: IDomain[], locale: string): IDomain => {
   const domain = domains.find((domain) => domainContainsLocale(domain, locale))
 
   if (typeof domain === "undefined") {
-    const error = new Error(`Domains in config didn't contain locale "${locale}".`)
-    logger.error(error)
-    throw error
+    throw new Error(`Domains in config didn't contain locale "${locale}".`)
   }
 
   return domain
