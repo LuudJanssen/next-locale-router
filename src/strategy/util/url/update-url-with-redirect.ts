ea9e5ff7b585b5b014e0e5b9faab0c02844cf07e
Luud Janssen <luud-@live.nl>
2021-04-19 15:34:27 +0200

    feat: replace uri-js with `url` library included in Next.js

--- src/strategy/util/url/update-url-with-redirect.ts
+++ src/strategy/util/url/update-url-with-redirect.ts
@@ -1,14 +1,14 @@
-import { parse, serialize } from "uri-js"
+import { format, parse } from "url"
 import { IRedirect } from "../../../util/redirect.interface"
 import { updatePathnameWithRedirect } from "./update-pathname-with-redirect"
 
 export const updateUrlWithRedirect = (url: string, redirect: IRedirect) => {
   const uri = parse(url)
 
-  const path = updatePathnameWithRedirect(uri.path, redirect)
+  const pathname = updatePathnameWithRedirect(uri.path, redirect)
 
-  return serialize({
+  return format({
     ...uri,
-    path,
+    pathname,
   })
 }
